f10c79318fbb880194fa4b7c477a60a9893d7b57
josdem <joseluis.delacruz@gmail.com>
2021-07-05 10:44:21 -0400

    Adding soap request by file

---
+++ soapRequestByFile.groovy
@@ -0,0 +1,14 @@
+@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
+
+import wslite.soap.SOAPClient
+
+File file = new File('request.xml')
+assert file.exists()
+
+String url = 'http://localhost:9090/codenotfound/ws/helloworld'
+
+def client = new SOAPClient(url)
+def response = client.send(file.text)
+
+print response.text
+assert 200 == response.httpResponse.statusCode
